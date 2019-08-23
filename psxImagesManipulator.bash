fa4c5df5fcf096a1b8eb1c7c2fa2bfc8557901f4
otanein <antoine@giannini.lol>
2019-08-23 17:45:49 +0200

    Add files via upload

---
+++ psxImagesManipulator.bash
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+moveFiles()
+{
+
+IFS=$'\n'
+for i in $(find | grep -i "$GAMENAME")
+do
+	chmod 644 "$i"
+	mv "$i" .tmp/
+done
+
+cd .tmp/
+
+}
+
+cueGen() 
+{
+
+for b in *
+do
+	echo $'\x02' | cmp -i 0:15 -n 1 - "$b"
+	case $? in
+	  0)
+	    MODE=MODE2
+	    ;;
+	  1)
+	    MODE=MODE1
+	    ;;
+	  *)
+	    echo "Invalid file format: $b"
+	    exit 1
+	esac
+
+	NAME=$(basename -- "$b")
+	OUTPUT="${NAME}.cue"
+	echo "FILE \"${NAME}\" BINARY
+	  TRACK 01 ${MODE}/2352
+	    INDEX 01 00:00:00" > "${OUTPUT}" &&
+	  echo "CUE file generated: ${OUTPUT}" ||
+	  echo "Failed to generate CUE file for $b"
+done
+
+}
+
+runSolo()
+{
+
+echo "How do you want to rename the image (extension will be automatically set to .chd) ?"
+read RENAME
+
+cueGen
+
+for i in *.cue
+do
+        chdman createcd -i "$i" -o "$i.chd"
+done
+
+for i in *.cue.chd
+do
+	mv "$i" "$RENAME".chd
+done
+
+if [[ $? -eq 0 ]]; then
+	CURDIR=$(pwd | awk -F "/" '{ print $NF }')
+	if [[ $CURDIR == ".tmp" ]]; then
+        	rm -f *.bin
+        	rm -f *.img
+        	rm -f *.cue
+	fi
+fi
+
+mv * ../.
+
+echo "My work here is done!"
+exit 0
+
+}
+
+runMulti()
+{
+
+CDNUM=0
+
+echo "How do you want to rename the files and playlist (extensions will be automatically set) ?"
+read RENAME
+
+cueGen
+
+for i in *.cue
+do
+        chdman createcd -i "$i" -o "$i.chd"
+done
+
+for i in *.cue.chd
+do
+        CDNUM=$[CDNUM + 1]
+	mv "$i" "$RENAME.CD$CDNUM"
+done
+
+if [[ $? -eq 0 ]]; then
+	CURDIR=$(pwd | awk -F "/" '{ print $NF }')
+	if [[ $CURDIR == ".tmp" ]]; then
+        	rm -f *.bin
+        	rm -f *.img
+        	rm -f *.cue
+	fi
+fi
+
+for i in *
+do
+        printf "$i"'%s\n' >> "$RENAME".m3u
+done
+
+mv * ../.
+
+echo "My work here is done!"
+exit 0
+}
+
+echo "What is the name of the files?
+Be careful to type the most common part of the files names for multi-discs games!
+Example : For ParasiteEve2 CD1.img and ParasiteEve2 CD2.img, type \"ParasiteEve2\" (No quotes or spaces)"
+read GAMENAME
+
+if [[ -d ./tmp ]]; then
+	mkdir .tmp/
+fi
+
+ls -1 |grep -i "$GAMENAME" > /dev/null 2>&1 ;
+RETVAL=$?
+
+if [[ $RETVAL -ne 0 ]]; then
+	echo "Error : The file does not exist, is it present in your current folder?"
+	exit 1
+fi
+
+FILENAME=$(find | grep -i "$GAMENAME")
+
+echo "The following files are going to be processed :
+$(ls -1 |grep -i "$GAMENAME")
+To confirm, press Y, to cancel, press C."
+read CHOICE
+case $CHOICE in
+        Y|y)
+        moveFiles
+        ;;
+        C|c)
+        echo "Cancelling..."
+	exit 0
+        ;;
+        *)
+        echo "Incorrect, type Y or C"
+        exit 1
+esac
+
+echo "Multi-disc (M) or Solo (S)?"
+read ANSWER
+case $ANSWER in
+        M|m)
+        runMulti
+        ;;
+        S|s)
+        runSolo
+        ;;
+        *)
+        echo $ANSWER
+        echo "Please type M or S"
+        exit 1
+esac
